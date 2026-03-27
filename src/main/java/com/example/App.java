025d924787327f24e35b27a21e8cc605d2bed0ed
Antonio G <antonio12garcia19@gmail.com>
2026-03-27 16:39:07 -0700

    Finished birthday lookup app

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,21 +1,47 @@
 package com.example;
 
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class App {
+
+    private static HashMap<String, String> birthdayMap = new HashMap<>();
+
+    public static void initializeMap(String fileName) {
+        try (FileReader reader = new FileReader(fileName)) {
+
+            JsonArray jsonData = JsonParser.parseReader(reader).getAsJsonArray();
+
+            for (JsonElement element : jsonData) {
+                JsonObject obj = element.getAsJsonObject();
+
+                String name = obj.get("name").getAsString();
+                String birthday = obj.get("birthday").getAsString();
+
+                birthdayMap.put(name, birthday);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error reading JSON file");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
-        HashMap<String, String> birthdayMap = new HashMap<>();
+        // IMPORTANT: this matches your current file location
+        initializeMap("src/main/java/com/example/birthday.json");
 
-        birthdayMap.put("Lupita", "January 5");
-        birthdayMap.put("Antonio", "March 12");
-        birthdayMap.put("Maria", "July 20");
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a name: ");
-        String name = scanner.nextLine();
+        String name = input.nextLine();
 
         if (birthdayMap.containsKey(name)) {
             System.out.println(name + "'s birthday is " + birthdayMap.get(name));
@@ -23,6 +49,6 @@ public class App {
             System.out.println("Birthday unknown");
         }
 
-        scanner.close();
+        input.close();
     }
 }
