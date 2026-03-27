a8e0bc59bbee82a7295ee81c14cfcb297703fcbb
Antonio G <antonio12garcia19@gmail.com>
2026-03-27 15:46:20 -0700

    final version of birthday project

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,28 @@
 package com.example;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        System.out.println("hello birthday project");
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        HashMap<String, String> birthdayMap = new HashMap<>();
+
+        birthdayMap.put("Lupita", "January 5");
+        birthdayMap.put("Antonio", "March 12");
+        birthdayMap.put("Maria", "July 20");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a name: ");
+        String name = scanner.nextLine();
+
+        if (birthdayMap.containsKey(name)) {
+            System.out.println(name + "'s birthday is " + birthdayMap.get(name));
+        } else {
+            System.out.println("Birthday unknown");
+        }
+
+        scanner.close();
     }
 }
